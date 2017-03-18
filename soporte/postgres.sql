2cace82080767be1527a5e8e53602f3e40339670
Ali Sira <alisirar@gmail.com>
2017-03-18 18:19:32 -0300

    Agregar archivos soporte para la cracion de la db

---
+++ soporte/postgres.sql
@@ -0,0 +1,31 @@
+﻿
+DROP TABLE IF EXISTS user_roles;
+DROP TABLE IF EXISTS users;
+CREATE  TABLE users (
+  user_id  SERIAL PRIMARY KEY,
+  user_name VARCHAR(45) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(60) NOT NULL ,
+  enabled smallint NOT NULL DEFAULT 1);
+  
+CREATE TABLE user_roles (
+  user_role_id SERIAL,
+  user_id integer NOT NULL,
+  role varchar(45) NOT NULL,
+  CONSTRAINT user_roles_pkey PRIMARY KEY (user_role_id),
+  CONSTRAINT uni_userid_role UNIQUE (role,user_id),    
+  CONSTRAINT fk_userid FOREIGN KEY (user_id)
+      REFERENCES public.users (user_id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION);
+
+INSERT INTO users(user_name,email,password,enabled)
+VALUES ('priya','abc@abc.com','$2a$04$CO93CT2ObgMiSnMAWwoBkeFObJlMYi/wzzOnPlsTP44r7qVq0Jln2', 1);
+INSERT INTO users(user_name,email,password,enabled)
+VALUES ('naveen','def@def.com','$2a$04$j3JpPUp6CTAe.kMWmdRNC.Wie58xDNPfcYz0DBJxWkucJ6ekJuiJm', 1);
+
+INSERT INTO user_roles (user_id, role)
+VALUES (001, 'ROLE_USER');
+INSERT INTO user_roles (user_id, role)
+VALUES (002, 'ROLE_ADMIN');
+INSERT INTO user_roles (user_id, role)
+VALUES (002, 'ROLE_USER');
---
+++ soporte/postgres_fix_column_names.sql
@@ -0,0 +1,105 @@
+﻿alter table comercial.public.product_offer add column id_product_offer int4; 
+alter table comercial.public.product_offer add column id_product int4; 
+update product_offer set  id_product_offer = "idProductOffer", id_product = "idProduct" 
+alter table comercial.public.product_offer drop column "idProduct";
+alter table comercial.public.product_offer drop column "idProductOffer";
+
+ALTER TABLE public.product_offer ADD PRIMARY KEY (id_product_offer) ;
+create sequence id_product_offer_seq  start with 1  increment by 1  maxvalue 99999  minvalue 1;
+ALTER TABLE public.product_offer alter column id_product_offer SET DEFAULT NEXTVAL('id_product_offer_seq') ;
+
+
+alter table public.products add column id_product int4; 
+update products set id_product = "idProduct";
+
+ALTER TABLE public.order_detail  DROP CONSTRAINT order_detail_id_product_fkey
+alter table public.products drop column "idProduct";
+
+ALTER TABLE public.products ADD PRIMARY KEY (id_product) ;
+create sequence id_product_seq  start with 1  increment by 1  maxvalue 99999  minvalue 1;
+ALTER TABLE public.products alter column id_product SET DEFAULT NEXTVAL('id_product_seq') ;
+
+alter table order_detail add CONSTRAINT order_detail_id_product_fkey FOREIGN KEY (id_product)
+      REFERENCES public.products (id_product) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION;
+      
+CREATE OR REPLACE VIEW public.view_products AS 
+ SELECT ps.id_product,
+    ps.name,
+    ps.description,
+    ps.code,
+    ps.id_color,
+    ps.price,
+    ps.stock,
+    ps.measure,
+    ps.id_category,
+    ps.status,
+    ps.id_image,
+    ps.created_at,
+    ps.updated_at,
+    ps.material,
+    ps.finish,
+    ps.id_purpose,
+    ps.size,
+    ps.item_box,
+    ps.kg_meter,
+    ps.id_enviroment,
+    ps.price_sample,
+    ps.name_clean
+   FROM ( SELECT products.id_product,
+            products.name,
+            products.description,
+            products.code,
+            products.id_color,
+            products.price,
+            products.stock,
+            products.measure,
+            products.id_category,
+            products.status,
+            products.id_image,
+            products.created_at,
+            products.updated_at,
+            products.material,
+            products.finish,
+            products.id_purpose,
+            products.size,
+            products.item_box,
+            products.kg_meter,
+            products.id_enviroment,
+            products.price_sample,
+            translate(products.name::text, 'áéíóúÁÉÍÓÚäëïöüÄËÏÖÜñÑ'::text, 'aeiouAEIOUaeiouAEIOUNN'::text) AS name_clean
+           FROM products) ps;
+
+ALTER TABLE public.view_products
+  OWNER TO postgres;
+
+
+-- Table: public.relationated_product
+
+-- DROP TABLE public.relationated_product;
+
+alter TABLE public.relationated_product add column id_relationated_product int4; 
+alter TABLE public.relationated_product add column id_product int4; 
+alter TABLE public.relationated_product add column id_product_relation int4; 
+
+update public.relationated_product set id_relationated_product = "idRelationatedProduct", id_product =   "idProduct" , id_product_relation =  "idProductRelation";
+
+ALTER TABLE public.relationated_product  DROP CONSTRAINT relationated_product_pkey
+
+alter table public.relationated_product drop column "idRelationatedProduct";
+alter table public.relationated_product drop column "idProduct";
+alter table public.relationated_product drop column "idProductRelation";
+
+ALTER TABLE public.relationated_product ADD PRIMARY KEY (id_relationated_product) ;
+create sequence id_relationated_product_seq  start with 1  increment by 1  maxvalue 99999  minvalue 1;
+ALTER TABLE public.relationated_product alter column id_relationated_product SET DEFAULT NEXTVAL('id_relationated_product_seq') ;
+
+
+
+
+
+
+select * from relationated_product
+
+select * from offer
+select * from products

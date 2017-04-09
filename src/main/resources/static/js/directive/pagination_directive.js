8659af254ea6c8680895d8cc0d48d874f4350c65
Ali Sira <alisirar@gmail.com>
2017-04-08 23:24:42 -0300

    pagination view and directive 85%

--- src/main/resources/static/js/directive/pagination_directive.js
+++ src/main/resources/static/js/directive/pagination_directive.js
@@ -11,10 +11,7 @@ App.directive('pagingObject', function(){
     		scope.pagination = function() {
 
     			scope.limitBorder = {"left":12, "right":12};
-    			
     			scope.numPage =  Math.ceil(scope.model.count / scope.model.perPage);
-    			
-    			//console.log(scope.numPage);
     			
     			if (scope.numPage > 1){
     				element.find('.prev').removeClass('hidden');
@@ -28,94 +25,53 @@ App.directive('pagingObject', function(){
     			
     			for (var i=1;i<=scope.numPage;i++) {
     				scope.aryPagination.push(i);
-    			}    			
-    			
-    			scope.findModel();
-    			
+    			}
 
-    			/*if (skip > 0) {
-                    element.find('.prev').removeClass('hidden');
-                } else {
-                    element.find('.prev').addClass('hidden');
-                }
-
-                var nextSkip = parseInt(scope.params.skip) + parseInt(scope.params.limit);
-
-                if (nextSkip >= scope.count) {
-                    element.find('.next').addClass('hidden');
-                } else {
-                    element.find('.next').removeClass('hidden');
-                }*/
-    			
-    			
     		};
     		
     		
             scope.goToPage = function(page, $event) {
 
             	element.find('.active').removeClass('active');
-            	//$event.currentTarget.parentNode.className ='active'; //This is another way to set de class           	
+            	//$event.currentTarget.parentNode.className ='active'; //This is another way to set the class           	
             	angular.element($event.currentTarget).parent().addClass('active');
             	
-
-                /*if (scope.params.limit > scope.count) {
-                    scope.params.skip = 0;
-                } else {
-                    scope.params.skip = (page * scope.params.limit) - scope.params.limit;
-                }
-
-                scope.actionFind();
-                */
+            	scope.goToPageEvent = true;
         		scope.model.page = page;
-        		//scope.pagination();
         		scope.findModel();
-        		
         		
             };
     		
             //Evento que observa el cambio en la variable pero q la primera vez tiene el mismo valor averiguar por que
     		scope.$watch('model.perPage', function(perPage, perPageOld) {
-            	//console.log(perPage + '-' + perPageOld + '-' + scope.model.count);
+            	console.log(perPage + '-' + perPageOld + '-' + scope.model.count);
                 if (perPage!= perPageOld) {
                 	scope.model.page = 1;
-                	element.find('.active').removeClass('active');
                 }
             	
             	if (perPage > 0 && scope.model.count > 0) {
             		//console.log('paginacion1');
-            		//console.log(perPage + '-' + perPageOld + '-' + scope.products.count);            		
+            		//console.log(perPage + '-' + perPageOld + '-' + scope.products.count);
+            		scope.findModel();
             		scope.pagination();
             	}
             	
             });
     		
     		scope.$watch('model.count', function(modelCount, modelCountOld) {
-            	//console.log(modelCount + '-' + modelCountOld + '-' + scope.model.perPage);
-                /*if (perPage!= perPageOld) {
-                	//console.log('diferete');
-                    //scope.pagination();
-                }*/
-            	
-            	//scope.pagination();
-            	
             	if (scope.model.perPage > 0 && modelCount > 0) {
-            		//console.log('paginacion2');
-            		//console.log(modelCount + '-' + modelCountOld + '-' + scope.model.perPage  );
             		scope.pagination();
             	}
-            	
-            	
-            });    		
-    		
-            
+
+            });
+
             //Metodo q funciona cuando inluyo el atributo per-page ="{{products.perPage}}" dentro de la etiqueta <paging-object></paging-object> 
             //me parece un poco mas invasivo pero se ejecuta primero que el watch 
             /*attrs.$observe('perPage', function(values){
             	console.log(values);
             });
             */
-            
-    		
+
     	}
     }
 });

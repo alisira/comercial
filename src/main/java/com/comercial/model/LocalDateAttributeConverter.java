57bf36d2cb4e74cd02c22f60a789a302c5727c9b
Ali Sira <alisirar@gmail.com>
2017-04-11 12:02:05 -0300

    include utility classes to service and domain layer

---
+++ src/main/java/com/comercial/model/LocalDateAttributeConverter.java
@@ -0,0 +1,34 @@
+package com.comercial.model;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+import javax.persistence.Entity;
+
+import org.springframework.data.annotation.Transient;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+@Entity
+@Converter(autoApply = true)
+public class LocalDateAttributeConverter implements AttributeConverter<LocalDateTime, Timestamp> {
+
+	@Override
+    @Transient
+	public Timestamp convertToDatabaseColumn(LocalDateTime attribute) {
+		return (attribute == null ? null : Timestamp.valueOf(attribute));
+	}
+
+	@Override
+    @Transient
+	public LocalDateTime convertToEntityAttribute(Timestamp dbData) {
+		return (dbData == null ? null : dbData.toLocalDateTime());
+	}
+
+
+
+
+}
---
+++ src/main/java/com/comercial/service/ServiceUtil.java
@@ -0,0 +1,21 @@
+package com.comercial.service;
+
+import java.beans.FeatureDescriptor;
+import java.util.stream.Stream;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
+
+public class ServiceUtil extends BeanUtils{
+
+    public static String[] getNullPropertyNames(Object source) {
+        final BeanWrapper wrappedSource = new BeanWrapperImpl(source);
+        return Stream.of(wrappedSource.getPropertyDescriptors())
+                .map(FeatureDescriptor::getName)
+                .filter(propertyName -> wrappedSource.getPropertyValue(propertyName) == null)
+                .toArray(String[]::new);
+    }
+	
+	
+}

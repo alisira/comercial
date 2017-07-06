efd9c60c150127b15fb21c7e753be157a18deb81
Ali Sira <alisirar@gmail.com>
2017-07-06 18:36:41 -0400

    nueva forma de manejar mensajes de error

--- src/main/resources/static/js/service/error_service.js
+++ src/main/resources/static/js/service/error_service.js
@@ -1,57 +1,33 @@
 'use strict';
 
-App.factory('ErrorService', ['$rootScope','$timeout',  function($rootScope,$timeout){
+App.factory('ErrorService', ['$rootScope','$timeout',  function($rootScope, $timeout){
 
 	var errorService = {};
 	
 	errorService.set = function(responseError){
 
 		//console.log(responseError);
-		if (!$rootScope.errors){
-			$rootScope.errors = [];	
-		}
-		
-		for (var error in $rootScope.errors){
-			if ($rootScope.errors[error] == "Disculpe las molestia, ocurrio el siguiente error: " +  responseError.data.message) 
-				var sw = true;
-		}
-		
-		if (!sw) $rootScope.errors.push("Disculpe las molestia, ocurrio el siguiente error: " +  responseError.data.message);
-		
-		$timeout(errorService.resetErrors, 9000);
-		
+		errorMessage = "Disculpe las molestia, ocurrio el siguiente error: " +  responseError.data.message;		
+		$rootScope.alerts = [ { type: 'danger', msg: errorMessage } ];
+
+		//$timeout(errorService.resetErrors, 9000);
+
 	};
-	
-	
+
 	errorService.setFormError = function(responseError){
 
-		//console.log(responseError);
-		if (!$rootScope.errors){
-			$rootScope.errors = [];	
-		}
-		
-		for (var error in $rootScope.errors){
-			if ($rootScope.errors[error] == responseError) 
-				var sw = true;
-		}
-		
-		if (!sw) $rootScope.errors.push(responseError);
-		
-		$timeout(errorService.resetErrors, 9000);
-		
+		$rootScope.alerts = [ { type: 'danger', msg: responseError } ];
+
+		//$timeout(errorService.resetErrors, 9000);
+
 	};
-	
-	
+
 	$rootScope.closeAlert = function(index) {
 		$rootScope.alerts.splice(index, 1);
 	};
-	
-	errorService.get = function(param){
-		return errors;
-	};
 
 	errorService.resetErrors = function() {
-		$rootScope.errors = null;
+		$rootScope.alerts = [];
     }
 
     return errorService;
--- src/main/resources/static/js/service/message_service.js
+++ src/main/resources/static/js/service/message_service.js
@@ -1,36 +1,20 @@
 'use strict';
 
-App.factory('MessageService', ['$rootScope','$timeout',  function($rootScope,$timeout){
+App.factory('MessageService', ['$rootScope','$timeout',  function($rootScope, $timeout){
 
 	var messageService = {};
 	
 	messageService.set = function(responseMessage){
 
 		//console.log(responseMessage);
-		if (!$rootScope.messages){
-			$rootScope.messages = [];	
-		}
+		$rootScope.alerts = [ { type: 'success', msg: responseMessage } ];		
 		
-		for (var message in $rootScope.messages){
-			if ($rootScope.messages[message] == responseMessage) 
-				var sw = true;
-		}
-		
-		if (!sw) $rootScope.messages.push(responseMessage);
-		
-		$timeout(messageService.resetMessages, 9000);
-		
-	};	
-	
-	messageService.get = function(param){
-		return messages;
 	};
 	
 	messageService.resetMessages = function() {
-		$rootScope.messages = null;
+		$rootScope.alerts = [];
     }
 
     return messageService;
 
 }]);
-
